9b48259472b601b1ab731cc100fcfd25488cb81c
Felicien Amafin <felicien.amafin@gmail.com>
2025-01-15 16:05:02 +0100

    remove firebase api key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,21 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 
-/* const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API,
-  authDomain: import.meta.env.FIREBASE_AUTH,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID,
-}; */
 const firebaseConfig = {
-  apiKey: "AIzaSyDuZPnlKJFgzhgVeW0ItOAixJIa5mwWyMk",
-  authDomain: "dashboard-700d3.firebaseapp.com",
-  projectId: "dashboard-700d3",
-  storageBucket: "dashboard-700d3.firebasestorage.app",
-  messagingSenderId: "568229419669",
-  appId: "1:568229419669:web:508ec9b5294342aed1999c"
+  apiKey: import.meta.env.VITE_FIREBASE_API,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
